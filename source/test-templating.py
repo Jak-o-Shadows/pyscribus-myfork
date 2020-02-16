c358e766e8d57f844e41401f29991f0e0fc26dc1
Etienne Nadji <etnadji@eml.cc>
2020-02-16 18:17:47 +0100

    Tests : Table cell stories templating 	modifié :         test-templating.py

--- source/test-templating.py
+++ source/test-templating.py
@@ -40,6 +40,10 @@ if __name__ == "__main__":
             "%Text%": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             "%Following%": "p. 5"
         },
+        {"%Cell1%": "First line"},
+        {"%Cell2%": "Second line"},
+        {"%Cell3%": "Third line"},
+        {"%Cell4%": "Last line"},
     ]
 
     # Get all stories of the template that contains
