406d739c0359e3831d204a18475f86bb0407b522
Etienne Nadji <etnadji@eml.cc>
2020-02-16 04:37:28 +0100

    Tests : Looking on SLA contents 	nouveau fichier : test-inspect.py

---
+++ source/test-inspect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding:Utf-8 -*-
+
+"""
+"""
+
+import pyscribus.sla as sla
+
+if __name__ == "__main__":
+    slafile = sla.SLA("tests/images.sla")
+
+    print(slafile.images())
+    print(slafile.pageobjects("image"))
+
+# vim:set shiftwidth=4 softtabstop=4:
