04b8aed9a27784201bbcf6f5c49fb8bfa83609eb
Etienne Nadji <etnadji@eml.cc>
2020-11-21 18:03:53 +0100

    Package version fix 	modifié :         CHANGELOG 	modifié :         source/setup.py

--- source/setup.py
+++ source/setup.py
@@ -6,7 +6,7 @@ import setuptools
 with open("../README.pypi.rst", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.2"
+VERSION = "0.2.1"
 DESCRIPTION = "Read, create and update Scribus .sla files."
 
 REQUIRED = ['lxml', 'svg.path']
